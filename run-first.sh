effbc0641dc9d1572fe442bc62ec020606da9265
Dominick Lemas <djlemas@login3.ufhpc>
2019-01-07 14:08:00 -0500

    first-run

--- run-first.sh
+++ run-first.sh
@@ -1,6 +1,25 @@
-fastQC v0.11.2+ 
-BBmap v36.92+ 
-Samtools v1.3.1 
-MetaPhlAn2 v2.0+ 
-QIIME v1.9.1+ 
-HUMAnN2 v0.9.9+ 
+ #!/bin/bash
+#SBATCH --job-name=demo
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=djlemas@ufl.edu
+#SBATCH --ntasks=2
+#SBATCH --mem=40gb
+#SBATCH --account=djlemas
+#SBATCH --qos=djlemas-b
+#SBATCH --time=1:00:00
+#SBATCH --output=demo_%j.log
+pwd; hostname; date
+
+# load modules
+module load bbmap
+module load nextflow
+module load fastqc         
+module load bbmap          
+module load samtools       
+module load metaphlan2     
+module load qiime          
+module load python/2.7.14 
+
+#Run nextflow script
+nextflow run YAMP.nf --reads1 ./data/ERR011089_1.fastq.gz --reads2 ./data/ERR011089_2.fastq.gz  --prefix Meta_HIT_ERR011089 --outdir ./data --mode complete
+date
