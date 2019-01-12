500e82814eea6ccbc507de59914588d9ce152b9b
Dominick Lemas <djlemas@login4.ufhpc>
2019-01-11 19:06:51 -0500

    ufrc update

--- run-01_getResources.sh
+++ run-01_getResources.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name=bbmap
+#SBATCH --job-name=bbmap_60min
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=djlemas@ufl.edu
 #SBATCH --ntasks=2
 #SBATCH --mem=40gb
 #SBATCH --account=djlemas
 #SBATCH --qos=djlemas-b
-#SBATCH --time=0:20:00
-#SBATCH --output=bbmap_%j.log
+#SBATCH --time=01:00:00
+#SBATCH --output=bbmap_60min_%j.log
 pwd; hostname; date
 
 # load modules
 module load bbmap
 
 # Download and uncompress files
-# wget https://zenodo.org/record/1068229/files/YAMP_resources_20171128.tar.gz
-tar -xzf /ufrc/djlemas/djlemas/DEMO-YAMP/YAMP/YAMP_resources_20171128.tar.gz
+#wget https://zenodo.org/record/1068229/files/YAMP_resources_20171128.tar.gz
+#tar -xzf /ufrc/djlemas/djlemas/DEMO-YAMP/YAMP/YAMP_resources_20171128.tar.gz
 
 #Run nextflow script
+cd /ufrc/djlemas/djlemas/DEMO-YAMP/YAMP/resources/
 bbmap.sh -Xmx40G ref=hg19_main_mask_ribo_animal_allplant_allfungus.fa.gz
-rm YAMP_resources_20171128.tar.gz
+# rm YAMP_resources_20171128.tar.gz
 date
